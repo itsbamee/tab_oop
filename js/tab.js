0b0f27905ec4fc12f3307d03c22be8ac167e200a
itsbamee <jan9239@naver.com>
2023-11-11 11:23:49 +0900

    클래스에 멤버변수등록 및 추가(추가설명 필요)

--- js/tab.js
+++ js/tab.js
@@ -1,15 +1,18 @@
 class Student {
+	//일일이 변경될 필요가 없는 공통의 값은 인스턴스 객체가 아니라 class 자체적으로 등록해서 전달가능 (멤버변수)
+	//아래 this.school로 등록 안해도 변수가 들어감
+
+	//public : 외부에서 변경가능한 멤버변수
+	// school = 'dcodelab';
+
+	//static : 외부에서 변경불가능한 멤버변수
+	//private : 외부에서 변경불가능하게 처리
+	#school = 'dcodelab';
+
 	constructor(name, age, gender) {
 		this.name = name;
 		this.age = age;
 		this.gender = gender;
-		this.school = 'dcodelab';
-	}
-	get school() {
-		return this.value;
-	}
-	set school(value) {
-		this.value = value === 'dcodelab' ? value : (value = 'dcodelab');
 	}
 }
 
