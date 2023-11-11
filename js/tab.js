4627ca062a51731cc5f654b3f0e73a9fa277f2ce
itsbamee <jan9239@naver.com>
2023-11-11 11:39:20 +0900

    tab 기본기능구현

--- js/tab.js
+++ js/tab.js
@@ -1,23 +1,21 @@
-class Student {
-	//일일이 변경될 필요가 없는 공통의 값은 인스턴스 객체가 아니라 class 자체적으로 등록해서 전달가능 (멤버변수)
-	//아래 this.school로 등록 안해도 변수가 들어감
+var tab = document.querySelector('#tab1');
+var btns = tab.querySelectorAll('ul li');
+var boxs = tab.querySelectorAll('section article');
 
-	//public : 외부에서 변경가능한 멤버변수
-	// school = 'dcodelab';
+btns.forEach(function (btn, idx) {
+	btn.addEventListener('click', function () {
+		// activation(btns, idx);
+		// activation(boxs, idx);
+		[btns, boxs].forEach(function (el) {
+			activation(el, idx);
+		});
+	});
+});
 
-	//static : 외부에서 변경불가능한 멤버변수
-	//private : 외부에서 변경불가능하게 처리
-	#school = 'dcodelab';
+function activation(arr, idx) {
+	arr.forEach(function (el) {
+		el.classList.remove('on');
+	});
 
-	constructor(name, age, gender) {
-		this.name = name;
-		this.age = age;
-		this.gender = gender;
-	}
+	arr[idx].classList.add('on');
 }
-
-const s1 = new Student('Emma', 20, 'famale');
-const s2 = new Student('Tom', 43, 'male');
-
-console.log(s1);
-console.log(s2);
