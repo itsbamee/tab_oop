f1828fbf57dc6cc6671bc4717537d282771d6f42
itsbamee <jan9239@naver.com>
2023-11-12 11:16:58 +0900

    모션 중 재이벤트 방지 흐름 주석 설명(eventBlock) 및 settimeout

--- js/tab_es6.js
+++ js/tab_es6.js
@@ -72,6 +72,13 @@ class Tab {
 		this.setHeight(idx);
 
 		//activeClass가 붙어서 활성화되고 fadeSpeed만큼 트랜지션 모션이 끝난 이후에 다시 eventBlock값을 false로 바꿔서 이벤트 연결 가능 처리
+		//모션이 끝날 때 까지 재클릭되면 안되도록 이전까지는 eventBlock값 true를 유지해서 이벤트를 막다가
+		//this.fadeSpeed 이후에 (this.eventBlock = false)를 읽어서 다시 실행되도록
+		//this.fadeSpeed(500,0.5초) 이후에 다시 eventBlock을 false로 변경해서 재이벤트 가능하게 처리
+		//1-eventBlock = false (초기 이벤트 가능한 상태)
+		//2-버튼 클릭 시 한번은 이벤트 발생시키고 순간적으로 eventBlock = true로 변경 (이 이후부터는 이벤트 발생 불가)
+		//3-내가 원하는 시점(모션이 끝나는 시점)에 다시 eventBlock=false로 변경 (이 때부터 다시 이벤트 발생가능)
+		//setTimeout =>  (실행함수=실행시간 후 동작할 내용 , 지연시간)
 		setTimeout(() => (this.eventBlock = false), this.fadeSpeed);
 	}
 
